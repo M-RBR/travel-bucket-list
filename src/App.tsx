667858ed9707971238d3937ce8c0ceb1574b3801
maximrubenbenjamin <roozenma@gmail.com>
2025-06-24 11:02:29 +0200

    Initialized Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
+import { app } from "./firebaseConfig";
 
 export default function App() {
+  console.log(app);
   return (
     <div className="min-h-screen w-full text-white">
       <Navbar />
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ_hALchblbYfT3MUWx7U_7RGc99XUaXg",
+  authDomain: "travel-bucket-list-d99b4.firebaseapp.com",
+  projectId: "travel-bucket-list-d99b4",
+  storageBucket: "travel-bucket-list-d99b4.firebasestorage.app",
+  messagingSenderId: "365854036878",
+  appId: "1:365854036878:web:dcaf52c1cbb359c59f8adf",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

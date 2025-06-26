0b0bbc6dd1cb76efb6ca7c53f8c239c7709dc921
maximrubenbenjamin <roozenma@gmail.com>
2025-06-26 14:08:22 +0200

    Initialized Cloud Firestore

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 console.log("[Firebase Config] Env Vars:", import.meta.env);
 console.log(
@@ -8,7 +8,6 @@ console.log(
   import.meta.env.VITE_FB_API_KEY || "UNDEFINED"
 );
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FB_API_KEY,
   authDomain: "travel-bucket-list-d99b4.firebaseapp.com",
@@ -18,8 +17,9 @@ const firebaseConfig = {
   appId: "1:365854036878:web:dcaf52c1cbb359c59f8adf",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);

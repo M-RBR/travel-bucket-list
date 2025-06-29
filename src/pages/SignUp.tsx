7ff9f703b4e479d879ab565ccece705c3a833539
maximrubenbenjamin <roozenma@gmail.com>
2025-06-29 21:28:36 +0200

    Made SignUp.tsx styling consistent with Login

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,19 @@
 import AuthSignUp from "../components/AuthSignUp";
+import { Link } from "react-router";
 
 export default function SignUp() {
   return (
     <div className="min-h-screen bg-gray-900 p-4">
-      <AuthSignUp />
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <div className="w-full max-w-md">
+            <Link to="/" className="block mb-8 text-center"></Link>
+            <div className="bg-gray-800 rounded-lg shadow-md overflow-hidden">
+              <AuthSignUp />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

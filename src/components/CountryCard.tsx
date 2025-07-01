9f32ae62c495b3960a863d69440f74546e2c1359
maximrubenbenjamin <roozenma@gmail.com>
2025-07-01 23:46:01 +0200

    Added close button for country details in bucklist card

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -24,22 +24,25 @@ export default function CountryCard({ country, onDelete }: Props) {
         className="w-full h-48 object-cover mb-3 rounded"
       />
 
-      <div className="flex flex-col gap-2 mt-4">
-        <Link
-          to={`/country/${encodeURIComponent(country.name.common)}`}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 rounded transition duration-200 text-center"
-        >
-          More Info
-        </Link>
-
-        {onDelete && (
-          <button
-            onClick={handleRemove}
-            className="bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 rounded transition duration-200"
+      <div className="flex flex-col gap-2 mt-4 items-center">
+        <div className="w-8/12 sm:w-3/4 flex flex-col gap-2">
+          <Link
+            to={`/country/${encodeURIComponent(country.name.common)}`}
+            state={{ fromBucketList: true }}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 rounded transition duration-200 text-center"
           >
-            Remove from Bucket List
-          </button>
-        )}
+            More Info
+          </Link>
+
+          {onDelete && (
+            <button
+              onClick={handleRemove}
+              className="bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 rounded transition duration-200"
+            >
+              Remove from Bucket List
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
